a4da6f9caa377c74f5d9cdde8adf8e10ccce0283
VishvaS1999 <gvishva2601@gmail.com>
2022-12-20 21:04:11 +0530

    Welcome Message

---
+++ src/com/oops/WelcomeMessage.java
@@ -0,0 +1,8 @@
+package com.oops;
+
+public class WelcomeMessage
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to The Object Oriented Programming!...");
+    }
+}
